0b233b7a07c019539b8041bca6f12daf9cf710f6
edwinbos <edwinbos@users.noreply.github.com>
2020-03-01 11:24:03 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,12 +18,12 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `WOD timer`,
+        short_name: `WOD timer`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
+        background_color: `#04e762`,
+        theme_color: `#04e762`,
+        display: `standalone`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
